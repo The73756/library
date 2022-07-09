ea7a189840d07dee6e98dfd459a786955f40f1f3
The73756 <the73756the@gmail.com>
2022-07-09 16:04:29 +0900

    add nav on use arrow and open use focus (dropdown)

--- src/js/lib/components/dropdown.js
+++ src/js/lib/components/dropdown.js
@@ -1,14 +1,50 @@
-function dropdown() {  
-	const triggers = document.querySelectorAll('[data-dd-target]');
+function dropdown() {
+	const trigger = document.querySelector('[data-dd-target]');
 
-	triggers.forEach(trigger => {
-		trigger.addEventListener('click', (e) => {
-			e.preventDefault();
-			let path = e.target.getAttribute('data-dd-target');
-			let menu = document.querySelector(`[data-dd-path="${path}"]`);
-			menu.classList.toggle('active');
+	let path = trigger.getAttribute('data-dd-target');
+
+	const menu = document.querySelector(`[data-dd-path="${path}"]`);
+	const menuItems = menu.querySelectorAll('li');
+	const menuLinks = menu.querySelectorAll('a');
+
+	trigger.addEventListener('focus', () => {
+		menu.classList.add('dropdown-menu--active');
+		let index = -1;
+
+		trigger.addEventListener('keydown', (e) => {
+			if (e.code === 'ArrowUp') {
+				index--;
+
+				if (index < 0) {
+					index = menuItems.length - 1;
+				}
+				for (let i = 0; i < menuItems.length; i++) {
+					menuItems[i].classList.remove('dropdown-menu__item--active');
+				}
+
+				menuItems[index].classList.add('dropdown-menu__item--active');
+			} 
+			
+			if (e.code === 'ArrowDown') {
+				index++;
+
+				if (index > menuItems.length - 1) {
+					index = 0;
+				}
+
+				for (let i = 0; i < menuItems.length; i++) {
+					menuItems[i].classList.remove('dropdown-menu__item--active');
+				}
+
+				menuItems[index].classList.add('dropdown-menu__item--active');
+			}
+
+			if (e.code === 'Enter' && menuLinks) {
+				menuLinks[index].click();
+			}
 		});
 	});
+
 }
 
 export default dropdown;
